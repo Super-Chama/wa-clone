dfbeca479bc653bac36d0bc8938ac1093a2d1955
Chamara Abesinghe <chamara@orangehrmlive.com>
2023-09-14 23:36:35 +0530

    setup vite, add layout

---
+++ src/App.vue
@@ -0,0 +1,28 @@
+<template>
+  <div class="wa-app">
+    <Layout></Layout>
+  </div>
+</template>
+
+<script lang="ts">
+import Layout from '@/components/Layout.vue'
+import { defineComponent } from 'vue'
+
+export default defineComponent({
+  name: 'app',
+
+  components: {
+    Layout
+  }
+})
+</script>
+
+<style lang="scss">
+html,
+body {
+  display: block;
+  box-sizing: border-box;
+  font-size: $base-font-size;
+  font-family: $base-font-family;
+}
+</style>
---
+++ src/components/Layout.vue
@@ -0,0 +1,20 @@
+<template>
+  <div class="wa-layout-container">
+    <aside class="wa-layout-sidepanel">
+      <slot name="navigator"></slot>
+    </aside>
+    <main class="wa-layout-content">
+      <slot name="body"></slot>
+    </main>
+  </div>
+</template>
+
+<script lang="ts">
+import { defineComponent } from 'vue';
+
+export default defineComponent({
+  name: 'layout'
+})
+</script>
+
+<style lang="scss" src="./layout.scss"></style>
